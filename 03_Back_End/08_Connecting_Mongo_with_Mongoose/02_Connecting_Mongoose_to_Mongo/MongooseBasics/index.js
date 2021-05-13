a2632e5f9f83bdafe28c5aa4a7d95b0f0c548ecc
SCDan0624 <dmayle1@hotmail.com>
2021-05-12 17:11:28 -0700

    completed mongoose find methods

--- 03_Back_End/08_Connecting_Mongo_with_Mongoose/02_Connecting_Mongoose_to_Mongo/MongooseBasics/index.js
+++ 03_Back_End/08_Connecting_Mongo_with_Mongoose/02_Connecting_Mongoose_to_Mongo/MongooseBasics/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/movieAPP", {
+  .connect("mongodb://localhost:27017/movieApp", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
